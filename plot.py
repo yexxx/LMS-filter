1d771fac3b6a57d3cf860573ceca5011adb93a7f
yexxx <yx19321@gmail.com>
2019-05-18 17:01:06 +0800

    2019.5.18

--- plot.py
+++ plot.py
@@ -8,9 +8,9 @@ def plot(length,dir,title):
     data,samplerate = sf.read(dir)
     data = np.array(data)
     d = data[rand:rand+length,rand1]
-    plt.figure(title)
+    plt.ylabel(title)
     plt.plot(d)
-    
+
 if(len(sys.argv)>1):
     length = int(sys.argv[1])
 else: 
@@ -19,7 +19,11 @@ else:
 data,_ = sf.read('Paragon.flac')
 rand = np.int(np.random.rand()*(data.shape[0]-length-1))
 rand1 = np.int(np.random.rand())
+plt.figure()
+plt.subplot(3,1,1)
 plot(length,'Paragon.flac',"Origin")
+plt.subplot(3,1,2)
 plot(length,'Paragon_with_noise.flac',"With_Noise")
+plt.subplot(3,1,3)
 plot(length,'Paragon_after_process.flac',"After_Process")
 plt.show()
